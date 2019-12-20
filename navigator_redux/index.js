7dee461524f32258c033138773ac5967faadf6ec
郭强 <guoqiang@guoqiangdeMacBook-Pro.local>
2019-12-20 20:53:48 +0800

    redux

--- navigator_redux/index.js
+++ navigator_redux/index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './js/App';
 import {name as appName} from './app.json';
 import AppNavigator from './js/Navigator/AppNavigator';
 import WelcomePage from './js/page/WelcomePage';
-AppRegistry.registerComponent(appName, () => AppNavigator);
+AppRegistry.registerComponent(appName, () => App);
